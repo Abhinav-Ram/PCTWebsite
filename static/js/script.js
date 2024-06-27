15f0d7a99e5bc8df4b19f9a07b51b85155c2542d
Abhinav Ram <abhinavram2002@gmail.com>
2024-06-27 11:44:01 +0530

    Bugfix: Menu Toggle on Window Size change

--- static/js/script.js
+++ static/js/script.js
@@ -1,4 +1,4 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const readMoreButtons = document.querySelectorAll('.read-more');
     const specialCardModal = document.getElementById('specialCardModal');
     const specialCardName = document.getElementById('specialCardName');
@@ -7,15 +7,29 @@ document.addEventListener('DOMContentLoaded', function() {
     const specialCardText = document.getElementById('specialCardText');
     const closeSpecialCardModal = document.getElementById('closeSpecialCardModal');
 
-    readMoreButtons.forEach(function(button) {
-        button.addEventListener('click', function() {
+    // JavaScript to toggle mobile menu
+    document.getElementById('burgerMenu').addEventListener('click', function () {
+        document.getElementById('mobileMenu').classList.toggle('hidden');
+    });
+
+    // Check window width on resize
+    window.addEventListener('resize', function () {
+        // If window width is greater than or equal to md breakpoint (768px in TailwindCSS)
+        if (window.innerWidth >= 768) {
+            // Ensure mobile menu is hidden
+            document.getElementById('mobileMenu').classList.add('hidden');
+        }
+    });
+
+    readMoreButtons.forEach(function (button) {
+        button.addEventListener('click', function () {
             const modalId = this.getAttribute('data-modal');
             const cardData = JSON.parse(this.getAttribute('data-card'));
-            
+            console.log("hi")
             specialCardName.textContent = cardData.name;
             specialCardText.textContent = cardData.details;
             specialCardImage.src = "https://t4.ftcdn.net/jpg/05/88/02/09/360_F_588020909_dPO6ZSbe5k3pQ8MZqut4drZWpiOM6Kbz.jpg"
-            
+
             // Send an Ajax request to the backend to fetch the image source
             fetch(`/get_image_source/${modalId}`)
                 .then(response => response.json())
@@ -29,7 +43,7 @@ document.addEventListener('DOMContentLoaded', function() {
         });
     });
 
-    closeSpecialCardModal.addEventListener('click', function() {
+    closeSpecialCardModal.addEventListener('click', function () {
         specialCardModal.classList.add('hidden');
     });
 });
